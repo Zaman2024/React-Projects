0254e7c6e8572dc4633eedabc234663efc82230d
Zaman <zamanofficial1980@gmail.com>
2024-06-30 00:13:40 +0530

    new project

---
+++ Todo-with-react-Redux/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-blue-700 p-1 text-xl text-white'>Zaman</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
