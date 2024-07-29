9e292b6d5368ad6e0671f30af74f42ab02f94faa
Zaman <zamanofficial1980@gmail.com>
2024-07-29 23:59:40 +0530

    adds codes

--- Todo-with-react-Redux/src/components/TodoDesign.jsx
+++ Todo-with-react-Redux/src/components/TodoDesign.jsx
@@ -3,12 +3,20 @@ import { useDispatch } from "react-redux";
 import { removeTodo, toggleInputForm } from "../feature/TodoSlice";
 
 function TodoDesign(props) {
+  const [done, setDone] = React.useState(false)
   const dispatch = useDispatch();
 
   return (
     <div className="flex justify-between items-center bg-yellow-600 px-2 rounded-lg">
-      <div>
-        <h1>{props.text}</h1>
+      <div className="flex">
+        <input 
+          type="checkbox"
+          onChange = {()=> setDone(!done)}
+          className="cursor-pointer"
+        />
+
+        <h1 className={`${done? 'line-through font-semibold':''}`}
+        >{props.text}</h1>
       </div>
       <div className="m-1">
         <button
