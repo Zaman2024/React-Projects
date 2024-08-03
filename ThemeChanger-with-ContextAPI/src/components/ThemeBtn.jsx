1bb7de941efd886604777fb87939a91549a782d8
Zaman <zamanofficial1980@gmail.com>
2024-08-04 03:00:51 +0530

    adds some new codes

--- ThemeChanger-with-ContextAPI/src/components/ThemeBtn.jsx
+++ ThemeChanger-with-ContextAPI/src/components/ThemeBtn.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import useTheme from '../ComtextAPI/ThemeContext'
 
 function ThemeBtn() {
+    const {themeMode, darkMode, lightMode}= useTheme()
+
+    const onBtnChange = (e) =>{
+        const darkModeStatus = e.currentTarget.checked;
+        if(darkModeStatus){
+            darkMode()
+        }else{
+            lightMode()
+        }
+    }
+
   return (
     <label>
         <input 
-            type="checkbox" 
+            type="checkbox"
+            value={'light'}
+            className='sr-only peer'
+            onChange={onBtnChange}
         />
-      
+      <span>
+        ThemeToggler
+      </span>
     </label>
   )
 }
