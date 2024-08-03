3c85d563acc6475e8d6f1ffeff2a32191127c6d7
Zaman <zamanofficial1980@gmail.com>
2024-08-03 21:17:48 +0530

    modified

---
+++ ThemeChanger-with-ContextAPI/src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <div>
+        <div>
+            fetch(https://api.github.com/users/Zaman2024)
+            .then(res = res.json())  
+        </div>
+        <div>
+            <h1>I am a passionate Web Developer</h1>
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default Home
