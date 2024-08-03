0e09476c12d123ae892c3954a3a774b6337d4556
Zaman <zamanofficial1980@gmail.com>
2024-08-04 03:26:27 +0530

    adds some new codes

--- ThemeChanger-with-ContextAPI/src/Navbar/Navbar.jsx
+++ ThemeChanger-with-ContextAPI/src/Navbar/Navbar.jsx
@@ -1,37 +1,39 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import ThemeBtn from "../components/ThemeBtn";
 
 function Navbar() {
   return (
-    <div
-      className='flex justify-between items-center bg-blue-700 text-white mb-8 px-4' 
-    >
-      <div className='rounded-full'>
-        <img src={'/public/Logo.jpg'} alt="Logo" width={52}
-        className='rounded-full m-2' 
+    <div className="flex justify-between items-center bg-blue-700 text-white mb-8 px-4">
+      <div className="rounded-full">
+        <img
+          src={"/public/Logo.jpg"}
+          alt="Logo"
+          width={52}
+          className="rounded-full m-2"
         />
       </div>
-      <ul
-        className='flex text-lg gap-12 font-semibold py-2'
-      >
-        <li>
-          <NavLink to = '/'>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to = 'About'>About</NavLink>
-        </li>
-        <li>
-          <NavLink to = 'Github'>Github</NavLink>
-        </li>
-        <li>
-          <NavLink to = 'Contact'>Contact</NavLink>
-        </li>
-      </ul>
-      
+      <div className="flex text-lg gap-12 font-semibold py-2">
+        <div>
+          <ThemeBtn/>
+        </div>
+        <ul className="flex text-lg gap-12 font-semibold">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="About">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="Github">Github</NavLink>
+          </li>
+          <li>
+            <NavLink to="Contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
-
+export default Navbar;
