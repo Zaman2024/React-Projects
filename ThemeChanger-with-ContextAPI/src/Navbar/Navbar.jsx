d32e2a18dd37f4fa5ff14296facf03abcbad0dd8
Zaman <zamanofficial1980@gmail.com>
2024-08-03 23:49:58 +0530

    modified codes

--- ThemeChanger-with-ContextAPI/src/Navbar/Navbar.jsx
+++ ThemeChanger-with-ContextAPI/src/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div>
+      <div>
+
+      </div>
+      <ul>
+        <li to='/'  />
+      </ul>
       
     </div>
   )
