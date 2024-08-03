b7300a2c4168795ce71fb8085016a132941830e4
Zaman <zamanofficial1980@gmail.com>
2024-08-03 22:23:25 +0530

    adds some new codes

--- ThemeChanger-with-ContextAPI/src/components/Home/Home.jsx
+++ ThemeChanger-with-ContextAPI/src/components/Home/Home.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 function Home() {
+  const [image, setImage] =useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Zaman2024')
+        .then(response => response.json())
+        .then(json => {
+          setImage(json.avatar_url)
+        })
+}, []);
+
   return (
     <>
-    <div>
-        <div>
-            fetch(https://api.github.com/users/Zaman2024)
-            .then(res = res.json())  
+    <div className='flex flex-col justify-center items-center m-auto border w-1/3 rounded-lg'>
+        <div 
+          className=''
+        >
+            <img src={`${image}`} alt='Image'/> 
         </div>
         <div>
             <h1>I am a passionate Web Developer</h1>
