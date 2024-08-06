b4b71a7d72fd160e610559e8a1723eb365b12eeb
Zaman <zamanofficial1980@gmail.com>
2024-08-07 02:06:34 +0530

    adds some new codes

--- Theme-changer/src/components/Card.jsx
+++ Theme-changer/src/components/Card.jsx
@@ -13,9 +13,11 @@ useEffect( () => {
 })
 
   return (
-    <div className='flex flex-col justify-center items-center m-auto border-2 border-black w-1/3 p-5 rounded-lg'>
+    <div className='flex flex-col justify-center items-center m-auto border-2 border-black w-1/3 p-5 rounded-lg bg-white dark:bg-slate-600 text-black dark:text-white'>
         <div>
-            <img src={image} alt='Image'/>
+            <img src={image} alt='Image'
+            className='border-2 border-black rounded-lg'
+            />
         </div>
         <div>
             <h1>This is a practice of Context Api</h1>
