16d403f2bcb60b2f44eddabaaa16c3c5114fd69a
Zaman <zamanofficial1980@gmail.com>
2024-08-08 23:20:23 +0530

    modified

--- Theme-changer/src/components/ThemeBtn.jsx
+++ Theme-changer/src/components/ThemeBtn.jsx
@@ -22,9 +22,8 @@ function ThemeBtn() {
             checked ={themeMode === 'dark'}
             className='sr-only peer'
         />
-        <div className="w-14 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-[189%] peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        <div className="w-14 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-[165%] peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         <span className=' text-sm font-medium text-gray-900'></span>
-      
     </label>
   );
 }
