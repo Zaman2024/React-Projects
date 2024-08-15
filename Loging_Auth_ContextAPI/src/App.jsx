0e3ef949eb7db748a9db9b837324ec9b90929116
Zaman <zamanofficial1980@gmail.com>
2024-08-15 17:07:59 +0530

    new codes

--- Loging_Auth_ContextAPI/src/App.jsx
+++ Loging_Auth_ContextAPI/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
+import Loging from './Components/Loging'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 
-        className='flex justify-center items-center m-auto w-full h-screen bg-slate-900 text-white text-xl font-bold'>
-          Mobile Star
-      </h1>
-    </>
+    <div>
+      <Loging/>
+    </div>
   )
 }
 
---
+++ Loging_Auth_ContextAPI/src/Components/Loging.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from 'react'
+import userContext from '../Context/userContext'
+
+function Loging() {
+    const [userName, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const {setUser} = useContext(userContext)
+
+const handleSubmit = (e) => {
+    e.preventDefault ();
+    setUser({userName, password})
+}
+  return (
+    <div>
+        <h1>Loging</h1>
+        <div>
+            <input
+                type='text'
+                value={userName}
+                onChange={e = setUsername(e.target.value)}
+                placeholder='Username'
+            />
+        </div>
+            <input
+                type='password'
+                value={password}
+                onChange={e = setPassword(e.target.value)}
+                placeholder='Password'
+            />
+        <div>
+            <button onClick={handleSubmit}>Submit</button>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Loging
