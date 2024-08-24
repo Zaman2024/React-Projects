c3de324b7b62987a31a87ac7f3a0a0613306784b
Zaman <zamanofficial1980@gmail.com>
2024-08-24 21:27:01 +0530

    new codes added

--- React-Megha-Project/src/appwrite/appwritrConfig.js
+++ React-Megha-Project/src/appwrite/appwritrConfig.js
@@ -33,15 +33,49 @@ async createPost({title, slug, content, featuredImage, status, userId}){
     }
 
 }
-
+    //--- Post Update ------
 async updatePost (slug,{title, content, featuredImage, status}) {
     try {
-        await this.databases.updateDocument()
+        return await this.databases.updateDocument(
+            config.AppwriteDatabaseId,
+            config.AppwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status
+            }
+        )
     } catch (error) {
         console.log("Appwrite serive :: getCurrentUser:: error" , error);
     }
 }
-
+    //--- Post Delete ---
+async deletePost ({slug}){
+    try {
+        await this.databases.deleteDocument(
+            config.AppwriteDatabaseId,
+            config.AppwriteCollectionId,
+            slug
+        )
+        return true
+    } catch (error) {
+        console.log("Appwrite service :: deletePost :: error",error);
+        return false
+    }
+}
+async getPost( slug){
+    try {
+        return await this.databases.getDocument(
+            config.AppwriteDatabaseId,
+            config.AppwriteCollectionId,
+            slug
+        )
+    } catch (error) {
+        console.log("Appwrite service :: getPost:: error",error)
+    }
+}
 }
 
 const appwriteService = new Service()
