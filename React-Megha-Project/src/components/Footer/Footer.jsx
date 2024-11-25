30fc819363156b2329ae59b521fb1d89f7f94ac8
Zaman <zamanofficial1980@gmail.com>
2024-11-25 17:39:15 +0530

    new code added

--- React-Megha-Project/src/components/Footer/Footer.jsx
+++ React-Megha-Project/src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from './Logo'
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <section className=''>
+
+    </section>
   )
 }
 
--- React-Megha-Project/src/main.jsx
+++ React-Megha-Project/src/main.jsx
@@ -8,7 +8,7 @@ import store from './Store/store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </StrictMode>,
 )
